a02285ae250e4bf653d7d56728417861373ba196
drone <simon.martinez.rzs@gmail.com>
2022-03-24 12:55:39 +0100

    Fixing Launch Localization experiments DLL

--- scripts/start_drone.sh
+++ scripts/start_drone.sh
@@ -8,11 +8,11 @@ sudo ip addr add 192.168.10.1/24 dev eno1
 sleep 1
 sudo ip link set eno1 up
 sleep 1
-sudo dnsmasq -C /dev/null -kd -F 192.168.10.20,192.168.10.21 -i eno1 --bind-dynamic
+sudo dnsmasq -C /dev/null -kd -F 192.168.10.20,192.168.10.21 -i eno1 --bind-dynamic &
 sleep 8
 
-roslaunch drone_launchers sdk.launch &
-roslaunch marsupial_launchers os1_sensor.launch 
+roslaunch marsupial_launchers sdk.launch &
+roslaunch marsupial_launchers os1_sensor.launch  
 
 
 # roslaunch ouster_ros os1.launch os1_hostname:=10.5.5.97 os1_udp_dest:=10.5.5.1 lidar_mode:=1024x20 
