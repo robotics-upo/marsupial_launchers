7d3918789025f485402371cf234cb0b99487b1e4
David <simon.martinez.rzs@gmail.com>
2022-07-14 20:24:25 +0200

    Added n_downsample. Not tracker in service

--- scripts/start_drone.sh
+++ scripts/start_drone.sh
@@ -17,5 +17,9 @@ roslaunch marsupial_launchers sdk.launch &
 roslaunch marsupial_launchers sensors.launch  &
 
 sleep 10
-roslaunch marsupial_launchers dll.launch
+roslaunch marsupial_launchers dll.launch &
+
+#sleep 5
+#roslaunch matrice_traj_tracker matrice_marsupial_traj_tracker.launch
+# Better to launch it separately by now and monitor!
 
