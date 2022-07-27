84fe4a35ef72485e874360864e39592f5d14096a
David <simon.martinez.rzs@gmail.com>
2022-07-27 17:39:03 +0200

    Searchs for a master in srlmsi before start

--- scripts/start_drone.sh
+++ scripts/start_drone.sh
@@ -10,8 +10,18 @@ sleep 1
 sudo dnsmasq -C /dev/null -kd -F 192.168.10.20,192.168.10.21 -i eno1 --bind-dynamic &
 sleep 8
 
-roscore &
-sleep 2
+#Check if we have ping to the base station host:
+BASE_STATION=srlmsi
+sleep 10
+ping -c1 srlmsi
+if [ $? -eq 0 ]
+then # If there is ping, wait for ros master
+    export ROS_MASTER_URI=http://srlmsi:11311
+    until rostopic list ; do echo "Waiting for roscore..."; sleep 1; done
+else
+    roscore &
+    sleep 2
+fi
 roslaunch marsupial_launchers sdk.launch &
 roslaunch marsupial_launchers sensors.launch  &
 roslaunch matrice_traj_tracker matrice_marsupial_traj_tracker.launch &
