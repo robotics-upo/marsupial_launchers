4199aaa2da0f16a8587cf481c6764115763b11a2
david-alejo <churr.te@gmail.com>
2026-04-01 09:15:50 +0200

    Different missions

--- scripts/mission_tether.sh
+++ scripts/mission_tether.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-roslaunch marsupial_mission_interface mission_execution.launch map_name:=rellano45_withobst mission_file:=rellano45_1 \
+roslaunch marsupial_mission_interface mission_execution.launch map_name:=rellano45_withobst mission_file:=rellano45_1_length_ok \
           able_tracker_uav:=false able_tracker_tether:=true able_tracker_ugv:=false
---
+++ scripts/mission_uav.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+roslaunch marsupial_mission_interface mission_execution.launch map_name:=rellano45_withobst mission_file:=rellano45_2 \
+          able_tracker_uav:=true able_tracker_tether:=false able_tracker_ugv:=false
